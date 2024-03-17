12676019366a580a27b42e949a2bb31ef26df6be
Wanjah <hingasarah132@gmail.com>
2024-03-17 20:35:21 +0300

    InitialCommit: Setting up AfyaBora's structure

---
+++ afya.py
@@ -0,0 +1,36 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+def to_markdown(text):
+    text.replace('.',' *')
+    return Markdown(textwrap.indent(text, ' >', predicate= lambda _:True))
+
+
+#used to securely store your API key
+#from google. import userdata
+GOOGLE_API_KEY = 'AIzaSyANkz6P7l-zj_oQbci37quuv5p8L_R1Cuk'
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+#list models
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+
+model = genai.GenerativeModel('gemini-pro-vision')
+
+#underliyng disease
+prompt_1 = "diabetes"
+#allergies 
+prompt_2 = "lactose intolerant"
+#Dietery restirictions
+prompt_3="vegan"
+
+#%%time
+response = model.generate_content("Provide a breakfast, lunch and dinner meal plan for a preson with the following"+ prompt_1+","+ prompt_2+","+ prompt_3)
+display(to_markdown(response.text)) 
